436eed9228eb511f96ca9b510721510c181cca42
Riashat Islam <riashat.islam.93@gmail.com>
2017-09-08 11:33:09 -0400

    Add files via upload

--- TRPO/run_trpo_mujoco.py
+++ TRPO/run_trpo_mujoco.py
@@ -2,34 +2,22 @@
 # noinspection PyUnresolvedReferences
 import mujoco_py # Mujoco must come before other imports. https://openai.slack.com/archives/C1H6P3R7B/p1492828680631850
 from mpi4py import MPI
-from baselines.common import set_global_seeds
+from baselines.common import set_global_seeds, tf_util as U
 import os.path as osp
 import gym
 import logging
+import argparse
 from baselines import logger
-from mlp_policy import MlpPolicy
+from baselines.ppo1.mlp_policy import MlpPolicy
+import tensorflow as tf
 from baselines.common.mpi_fork import mpi_fork
 from baselines import bench
-from baselines.trpo_mpi import trpo_mpi
+import trpo_mpi
+
 import sys
-import argparse
-num_cpu=1
 
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--network_size", type=int, default=32)
-parser.add_argument("--network_activation", type=str, default="tanh")
-args = parser.parse_args()
-
-
-
-def train(env_id, num_timesteps, seed):
-    whoami  = mpi_fork(num_cpu)
-    if whoami == "parent":
-        return
+def train(env_id, num_timesteps, seed, policy_hid_size, vf_hid_size, activation_policy, activation_vf):
     import baselines.common.tf_util as U
-    logger.session().__enter__()
     sess = U.single_threaded_session()
     sess.__enter__()
 
@@ -39,26 +27,36 @@ def train(env_id, num_timesteps, seed):
     workerseed = seed + 10000 * MPI.COMM_WORLD.Get_rank()
     set_global_seeds(workerseed)
     env = gym.make(env_id)
-
-
     def policy_fn(name, ob_space, ac_space):
         return MlpPolicy(name=name, ob_space=env.observation_space, ac_space=env.action_space,
-            hid_size=args.network_size, num_hid_layers=2, activation=args.network_activation)
-
-
-    env = bench.Monitor(env, osp.join(logger.get_dir(), "%i.monitor.json" % rank))
+            policy_hid_size=policy_hid_size, vf_hid_size=vf_hid_size, activation_policy=activation_policy, activation_vf=activation_vf)
+    env = bench.Monitor(env, logger.get_dir() and 
+        osp.join(logger.get_dir(), "%i.monitor.json" % rank))
     env.seed(workerseed)
     gym.logger.setLevel(logging.WARN)
 
-    trpo_mpi.learn(env, policy_fn, timesteps_per_batch=1024, max_kl=0.01, cg_iters=10, cg_damping=0.1,
-        max_timesteps=num_timesteps, gamma=0.99, lam=0.98, vf_iters=5, vf_stepsize=1e-3)
+    trpo_mpi.learn(env, policy_fn, timesteps_per_batch=int(5e3), max_kl=0.01, cg_iters=20, cg_damping=0.1,
+        max_timesteps=int(num_timesteps), gamma=0.995, lam=0.97, vf_iters=5, vf_stepsize=1e-3)
     env.close()
 
-
-
-
 def main():
-    train('Hopper-v1', num_timesteps=1e6, seed=0)
+    parser = argparse.ArgumentParser(description='TRPO.')
+    parser.add_argument('--env', help='environment ID', default='HalfCheetah-v1')
+    parser.add_argument("--policy_size", nargs="+", default=(64,64), type=int)
+    parser.add_argument("--value_func_size", nargs="+", default=(64,64), type=int)
+    parser.add_argument("--activation_vf", type=str, default="tanh")
+    parser.add_argument("--activation_policy", type=str, default="tanh")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--log_dir", type=str, default="./Results/")
+    activation_map = { "relu" : tf.nn.relu, "leaky_relu" : U.lrelu, "tanh" :tf.nn.tanh}
+
+    args = parser.parse_args()
+    logger.configure(dir=args.log_dir)
+    activation_policy = activation_map[args.activation_policy]
+    activation_vf = activation_map[args.activation_vf]
+
+    train(args.env, 2e6, args.seed, args.policy_size, args.value_func_size, activation_policy,  activation_vf)
+
 
 if __name__ == '__main__':
     main()
